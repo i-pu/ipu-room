bf6d5e4d6d0b556261e4c6b768a0129d792e0285
wakame-tech <kamata1919@gmail.com>
2019-04-29 00:43:22 +0900

    #15 : Fix build error, Add local plugin simurator

--- client/src/components/room/Desk.vue
+++ client/src/components/room/Desk.vue
@@ -1,13 +1,25 @@
 
 <template lang="pug">
   #desk
-    v-btn(color="info" @click="onEnterRoom") EnterRoom
+    v-btn(color="info" :disabled="pluginLoaded" @click="onEnterRoomTest") ACTIVATE
     // v-btn(color="primary" @click="trigger") トリガー
 
     div(v-html="pluginHtml")
 </template>
 
 <script>
+// test in local
+class Plugin {
+  constructor () {
+    this.count = 0
+  }
+
+  plus (data) {
+    this.count += parseInt(data)
+    return { changed: 'count' }
+  }
+}
+
 export default {
   name: 'Desk',
   props: {
@@ -15,7 +27,11 @@ export default {
   },
   data () {
     return {
+      testInLocal: true,
+      plugin: null,
       pluginHtml: '',
+      pluginElements: [],
+      pluginLoaded: false
     }
   },
   sockets: {
@@ -29,25 +45,65 @@ export default {
   mounted() {
   },
   methods: {
-    loadPlugin ({ html }) {
-      console.log(html)
-      this.pluginHtml = html
-      const events = [
-        { id: 'a', name: 'plus' }
-      ]
-      this.$nextTick(() => {
-        for (let emitEvent of events) {
-          document.getElementById(emitEvent.id).addEventListener('click', () => {
-            console.log(`event: ${emitEvent.name} fired`)
-            this.$socket.emit('plugin/trigger', {
-              event: emitEvent.name,
-              plugin_id: 'counter',
-              room_id: this.room.room_id,
-              args: [1]
-            })
-          })
-        }
+    // test methods in local
+    trigger ({ event, args }) {
+      const { changed } = this.plugin[event](args)
+      this.pluginElements.filter(element => element.type === 'view' && element.bind === changed).forEach(element => {
+        const target = document.getElementById(element.id)
+        target.innerText = this.replacer(element)
       })
+    },
+
+    replacer (element) {
+      const [all, variable] = element.template.match(/\{\{(.*)\}\}/)
+      return element.template.replace(all, this.plugin[variable])
+    },
+    onEnterRoomTest () {
+      const html = `
+        <p id="_1"></p>
+        <button id="_2"> add </button>
+      `
+      const elements = [
+        { id: '_1', type: 'view', bind: 'count', template: '{{count}}' },
+        { id: '_2', type: 'event', event: 'plus' }
+      ]
+      this.activatePlugin({ html, elements })
+    },
+
+    activatePlugin ({ html, elements }) {
+      this.pluginElements = elements
+
+      const _attachEvent = (eventElement) => {
+        document.getElementById(eventElement.id).addEventListener('click', () => {
+          console.log(`event: ${eventElement.event} fired`)
+          const payload = {
+            event: eventElement.event,
+            plugin_id: 'counter',
+            room_id: this.room.room_id,
+            args: [1]
+          }
+          if (this.testInLocal) {
+            this.trigger(payload)
+          } else {
+            this.$socket.emit('plugin/trigger', payload)
+          }
+        })
+      }
+
+      this.plugin = new Plugin()
+      this.pluginHtml = html
+      this.$nextTick(() => {
+        // register event
+        elements.filter(element => element.type === 'event').forEach(element => {
+          _attachEvent(element)
+        })
+        // render view elements
+        elements.filter(element => element.type === 'view').forEach(element => {
+          const target = document.getElementById(element.id)
+          target.innerText = this.replacer(element)
+        })
+      })
+      this.pluginLoaded = true
     }
   },
 }
