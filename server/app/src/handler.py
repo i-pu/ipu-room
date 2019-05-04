1b7111edb370c3b11418de1f299c0cf68daf2c48
hamadakafu <kafu.h1998@gmail.com>
2019-05-04 16:33:13 +0900

    add handler room and plugin

--- server/app/src/handler.py
+++ server/app/src/handler.py
@@ -1,14 +1,24 @@
 from flask import jsonify
 
 from .config import app
-from .models import Room
+from .models import Room, Plugin
 
 
 @app.route('/room', methods=['GET'])
 def get_rooms():
-    rooms = Room.query().all()
+    rooms = Room.query.all()
     return jsonify(
         {
-            rooms: rooms
+            'rooms': list(map(Room.__to_dict__, rooms)),
+        }
+    )
+
+
+@app.route('/plugin', methods=['GET'])
+def get_plugins():
+    plugins = Plugin.query.all()
+    return jsonify(
+        {
+            'plugins': list(map(Plugin.__to_dict__, plugins)),
         }
     )
