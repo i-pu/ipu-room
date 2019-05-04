34b8c941cfe47c3d3b49c3ffafce6f9d60df4454
hamadakafu <kafu.h1998@gmail.com>
2019-05-04 16:35:00 +0900

    issue #16: add tests for handler
    /room
    /plugin

---
+++ server/app/tests/test_handler.py
@@ -0,0 +1,21 @@
+import unittest
+
+import requests
+
+
+class TestHandler(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.url = 'http://localhost'
+        cls.port = ':8000'
+
+    def test_room(self):
+        res = requests.get(self.url + self.port + '/room')
+        self.assertEqual(200, res.status_code)
+        self.assertEqual(type(res.json()['rooms']), list)
+
+    def test_plugin(self):
+        res = requests.get(self.url + self.port + '/plugin')
+        self.assertEqual(200, res.status_code)
+        self.assertEqual(type(res.json()['plugins']), list)
