3c00d340e99ebb9b9cfe76cd38ae66689369d556
hamadakafu <kafu.h1998@gmail.com>
2019-05-05 20:03:07 +0900

    issue #27: fix Dockerfile
    run uwsgi from uwsgi.ini

--- server/app/entry.sh
+++ server/app/entry.sh
@@ -4,15 +4,16 @@ set -ex
 
 cmd="$@"
 
-echo -n "*:*:*:*:$POSTGRES_PASSWORD" > ~/.pgpass
-chmod 600 ~/.pgpass
+# hide password from stdout
+set +ex
+export PGPASSWORD=$POSTGRES_PASSWORD
+set -ex
 
 until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -w -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
-rm -f ~/.pgpass
 
 >&2 echo "Postgres is up - executing command"
 exec $cmd
