031c2d47dccd6761a49c7ea8af5f3bedaf28b7db
hamadakafu <kafu.h1998@gmail.com>
2019-05-08 00:51:14 +0900

    issue #32: bug fixed 7 server-CD 多分最後(フラグ)

--- gcb/sed.sh
+++ gcb/sed.sh
@@ -10,7 +10,7 @@ image=$1-server
 curDir=$(cd $(dirname $0);pwd)
 filepath=${curDir}/kube/server-pod.yaml
 
-sed -e 's/PROJECT_ID/${projectid}/g' ${filepath}
-sed -e 's/DOCKER_USERNAME/${dockerusername}/g' ${filepath}
-sed -e 's/IMAGE/${image}/g' ${filepath}
-sed -e 's/TAG/${tag}/g' ${filepath}
+sed -i s/PROJECT_ID/${projectid}/g ${filepath}
+sed -i s/DOCKER_USERNAME/${dockerusername}/g ${filepath}
+sed -i s/IMAGE/${image}/g ${filepath}
+sed -i s/TAG/${tag}/g ${filepath}
