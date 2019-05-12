5983e2ebffe08405ef936406f2282ba86078eea6
hamadakafu <kafu.h1998@gmail.com>
2019-05-13 00:34:52 +0900

    add client-cd

---
+++ gcp/README.md
@@ -0,0 +1,7 @@
+# Tips
+- gcr
+    - ライフサイクルを設定してもすぐに反映されるわけではない
+- google cloud run
+    - ポートを環境変数として必要があるが
+    - その際に，80 ポートだとだめなので注意
+    - https://medium.com/google-cloud/google-cloud-run-or-how-to-run-your-static-website-in-5-minutes-and-much-more-dbe8f2804395
--- gcp/terraform/build_trigger.tf
+++ gcp/terraform/build_trigger.tf
@@ -1,15 +1,26 @@
+# Repository must be created beforehand
+
 resource "google_cloudbuild_trigger" "server" {
   trigger_template {
-    tag_name = "gcb"
+    tag_name = "server-cd"
     repo_name = "github_i-pu_ipu"
-    # Repository must be created beforehand
   }
 
-  substitutions = {
+  substitutions {
     _DOCKER_USERNAME = "${lookup(var.docker_secret, "username")}"
     _DOCKER_PASSWORD = "${lookup(var.docker_secret, "password")}"
     _ZONE = "${var.zone}"
   }
 
-  filename = "gcp/cloudbuild.yaml"
+  filename = "gcp/server-cd-cloudbuild.yaml"
+}
+
+resource "google_cloudbuild_trigger" "client" {
+  trigger_template {
+    tag_name = "client-cd"
+    repo_name = "github_i-pu_ipu"
+  }
+
+  substitutions {}
+  filename = "gcp/client-cd-cloudbuild.yaml"
 }
