9cbf0fe5d3d8a24ad00f73f1e165e990cbb86d0e
wakame-tech <kamata1919@gmail.com>
2019-06-11 15:22:43 +0900

    #48 : [Add] Auto reload

--- client/src/components/store/PluginEditor.vue
+++ client/src/components/store/PluginEditor.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
   div
     v-container(fluid grid-list-md text-xs-center)
+      v-layout(row wrap v-if="innerPluginPackage")
+        v-flex(d-flex xs12 sm12 md6)
+          code.blockquote.editor(@input="syncTemplate" v-text="editableTempate" contenteditable="true")
+        v-flex(d-flex xs12 sm12 md6)
+          code.blockquote.editor(@input="syncFunctions" v-text="editableFunctions" contenteditable="true")
       v-layout(row wrap)
-        v-flex(d-flex xs12 sm12 md9 v-if="innerPluginPackage")
-          v-textarea(
-            label="Plugin"
-            v-model="innerPluginPackage.plugin.template"
-          )
-        v-flex(d-flex xs12 sm12 md9 v-if="instance")
+        v-flex(d-flex xs12 sm12 md6 v-if="instance")
           v-card(white fluid)
             component(:is="instance.component")
 </template>
@@ -15,9 +15,9 @@
 <script lang="ts">
 import Vue from 'vue'
 import _ from 'lodash'
+
 import Component from 'vue-class-component'
 import { Prop, Watch } from 'vue-property-decorator'
-
 import { PluginPackage, PluginComponent, Room, PluginInstance } from '@/model'
 import { boot } from '@/logic/loader'
 
@@ -36,12 +36,35 @@ export default class PluginEditor extends Vue {
   private innerPluginPackage: PluginPackage | null = null
   private instance: PluginInstance | null = null
 
+  private editableFunctions: string = ''
+  private editableTempate: string = ''
+
   async mounted () {
     this.innerPluginPackage = _.clone(this.pluginPackage)
-    this.$socket.emit('room/make', { name: '部屋', pluginPackages: [ this.innerPluginPackage ] })  
+
+    this.editableTempate = _.clone(this.innerPluginPackage.plugin.template)
+    this.editableFunctions = this.prettifiedFunctions
+
+    this.$socket.emit('room/make', { name: '部屋', pluginPackages: [ this.innerPluginPackage ] }) 
   }
 
-  @Watch('innerPluginPackage.plugin.template')
+  private get prettifiedFunctions (): string {
+    return JSON.stringify(this.innerPluginPackage!!.plugin.functions, null, "\t")
+  }
+
+  private syncFunctions (e: Event) {
+    try {
+      this.innerPluginPackage!!.plugin.functions = JSON.parse((e.target as HTMLInputElement).innerHTML)
+    } catch (e) {
+      console.log('Systax Error')
+    }
+  }
+
+  private syncTemplate (e: Event) {
+    this.innerPluginPackage!!.plugin.template = (e.target as HTMLInputElement).innerText
+  }
+
+  @Watch('innerPluginPackage', { deep: true })
   private reloadPlugin () {
     if (!this.room) {
       return
@@ -57,3 +80,9 @@ export default class PluginEditor extends Vue {
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.editor
+  height 300px
+  text-align left
+</style>
