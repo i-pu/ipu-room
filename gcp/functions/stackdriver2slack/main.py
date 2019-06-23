28d8a347b3558be91c981d1f2d69efddeca8219d
hamadakafu <kafu.h1998@gmail.com>
2019-06-23 16:43:45 +0900

    WIP upgrade functions

--- gcp/functions/stackdriver2slack/main.py
+++ gcp/functions/stackdriver2slack/main.py
@@ -10,17 +10,34 @@ users = {
 }
 slack_channel = os.getenv('SLACK_CHANNEL')
 
+
 @auth.get_password
 def get_pw(username):
     if username in users:
         return users.get(username)
     return None
 
+
 @auth.login_required
 def entry_point(request):
-
     request_json = request.get_json()
-    for i in request_json.items():
-        print(i, flush=True)
+    incident = request_json['incident']
     slack = slackweb.Slack(url=slack_channel)
-    slack.notify(text='hello, auth: {}'.format(auth.username()))
+    if incident['state'] == 'open':
+        text = ('error happen!\n',
+                'incident_id: {}, resource: {}\n'.format(
+                    incident['incident_id'],
+                    incident['resource']
+                ),
+                'policy: {}, condition: {}\n'.format(
+                    incident['policy_name'],
+                    incident['condition_name'],
+                ),
+                '{}\n'.format(incident['document']['content']),
+                'summary: {}\n'.format(incident['summary']),
+                )
+    elif incident['state'] == 'closed':
+        text = (
+            'closed: incident_id: {}'.format(incident['incident_id'])
+        )
+    slack.notify(text=text, mrkdwn=True)
