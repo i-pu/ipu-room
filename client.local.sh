5e24d78c5b554c0c5b175c1cb3645008e1f9bf62
hamadakafu <kafu.h1998@gmail.com>
2019-06-25 13:16:19 +0900

    fixed bug on shell

---
+++ client.local.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+set -ex
+
+./helm3-alpha uninstall client || true
+docker build -t kafuhamada/ipu-client -f Dockerfile-cd client
+
+./helm3-alpha install \
+-f helm/values.yaml \
+-f helm/client/values.yaml \
+client helm/client
