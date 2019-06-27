8733eff151fbbed862e0a89486c9a00b1700fe6f
hamadakafu <kafu.h1998@gmail.com>
2019-06-27 14:12:54 +0900

    issue #101: room.rs のエラーハンドリング対応

--- database-controller/src/v1/room.rs
+++ database-controller/src/v1/room.rs
@@ -1,4 +1,4 @@
-use actix_web::{web};
+use actix_web::web;
 use diesel::{
     r2d2::{self, ConnectionManager},
     pg::PgConnection,
@@ -6,52 +6,51 @@ use diesel::{
     RunQueryDsl,
 };
 
-use crate::model::{Room};
+use crate::model::Room;
 
 type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-pub fn get_all_rooms(pool: web::Data<Pool>) -> web::Json<Vec<Room>> {
-    use crate::schema::rooms::dsl::rooms;
-    web::Json(rooms.load(&pool.get().unwrap()).unwrap())
+pub fn get_all_rooms(pool: web::Data<Pool>)
+                     -> Result<web::Json<Vec<Room>>, failure::Error> {
+    use crate::schema::rooms;
+    let rooms = rooms::dsl::rooms.load(&pool.get()?)?;
+
+    debug!("{}", serde_json::to_string(&rooms)?);
+    Ok(web::Json(rooms))
 }
 
-pub fn get_room(path: web::Path<String>, pool: web::Data<Pool>) -> web::Json<Room> {
-    use crate::schema::rooms::dsl::rooms;
+pub fn get_room(path: web::Path<String>, pool: web::Data<Pool>)
+                -> Result<web::Json<Room>, failure::Error> {
+    use crate::schema::rooms;
     let id = path.into_inner();
-    let room = rooms.find(&id).first(&pool.get().unwrap())
-        .expect(&format!("not found room: {:?}", id));
-    println!("{:#?}", room);
-    web::Json(room)
+    let room = rooms::dsl::rooms.find(&id).first(&pool.get()?)?;
+
+    debug!("{}", serde_json::to_string(&room)?);
+    Ok(web::Json(room))
 }
 
 /// create room
 pub fn post_room(json: web::Json<Room>, pool: web::Data<Pool>)
-                   -> web::Json<Room>
+                 -> Result<web::Json<Room>, failure::Error>
 {
-    /// todo: 返り値をResult にする
     use crate::schema::rooms::dsl::rooms;
-    let room: Room =
-        diesel::insert_into(rooms)
-            .values(&json.0)
-            .get_result(&pool.get().unwrap())
-            .unwrap();
+    let room: Room = diesel::insert_into(rooms)
+        .values(&json.0)
+        .get_result(&pool.get()?)?;
 
-    println!("{:#?}", room);
-    web::Json(room)
+    debug!("{}", serde_json::to_string(&room)?);
+    Ok(web::Json(room))
 }
 
 /// update room
 pub fn put_room(json: web::Json<Room>, pool: web::Data<Pool>)
-                  -> web::Json<Room>
+                -> Result<web::Json<Room>, failure::Error>
 {
-    use crate::schema::rooms::{dsl::*};
-    let mut new_room: Room = json.0;
-    new_room =
-        diesel::update(rooms.find(new_room.id.clone()))
-            .set(new_room)
-            .get_result(&pool.get().unwrap())
-            .unwrap();
+    use crate::schema::rooms::dsl::{rooms, id};
+    let new_room = diesel::update(rooms.find(json.id.clone()))
+        .set(json.0)
+        .get_result(&pool.get()?)?;
 
-    println!("{:#?}", new_room);
-    web::Json(new_room)
+    debug!("{}", serde_json::to_string(&new_room)?);
+    Ok(web::Json(new_room))
 }
