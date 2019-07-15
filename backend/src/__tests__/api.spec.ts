07b42ac252dad7b27ea5bb72cda7300bd044fc0e
wakame-tech <kamata1919@gmail.com>
2019-07-15 14:16:12 +0900

    #66 : [Clean] add test

--- backend/src/__tests__/api.spec.ts
+++ backend/src/__tests__/api.spec.ts
@@ -1,45 +1,17 @@
 // @ts-ignore
 import fetch from 'node-fetch'
-import Counter from '@/examples/counter'
-import { PluginMeta, PluginPackage } from '@model'
+import { PluginPackage, PluginMeta } from '@model'
+
+import Counter from '@examples/counter'
 
 const API_ORIGIN = 'http://localhost:3000/api/v1'
 
-let pluginId: string = ''
-
-describe('POST /market/plugins', () => {
-  it('plugin upload test', async () => {
-    // why {} ???
-    console.log(Counter)
-
-    const payload = await fetch(`${API_ORIGIN}/market/plugins`, {
-      method: 'POST',
-      body: JSON.stringify(Counter)
-    })
-      .then((res: Response) => res.json())
-
-    expect(payload.uploadedMeta)
-      .toHaveProperty('id')
-    
-    pluginId = payload.uploadedMeta.id
-  })
-})
-
-describe('GET /market/plugins/:id', () => {
-  it('plugin fetch meta test', async () => {
-    const meta: PluginMeta = await fetch(`${API_ORIGIN}/market/plugins/${pluginId}`)
-      .then((res: Response) => res.json())
-
-    expect(meta)
-      .toMatchObject({
-        id: expect.any(String)
-      })
-  })
-})
-
 describe('GET /plugin/load/:id', () => {
   it('plugin load test', async () => {
-    const pluginPackage: PluginPackage = await fetch(`${API_ORIGIN}/plugin/load/${pluginId}`)
+    const pluginPackage: PluginPackage = await fetch(`${API_ORIGIN}/plugin/compile`, {
+      method: 'POST',
+      body: JSON.stringify(Counter as PluginMeta)
+    })
       .then((res: Response) => res.json())
 
   expect(pluginPackage)
